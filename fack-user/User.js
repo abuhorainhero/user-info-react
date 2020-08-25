bc4b57388ab8e39bb5775da4f868af14a27408d5
abuhorainhero <abuhorain2016@gmail.com>
2020-08-25 20:37:22 +0600

    first

---
+++ fack-user/User.js
@@ -0,0 +1,135 @@
+var userInfo = [
+    {
+        "id":"1",
+        "name": "Abu Horain",
+        "phone": "01748008121",
+        "email": "abuhorain2016@gmail.com",
+        "salary": "15000",
+        "image": "http://"
+    },
+    {
+        "id":"2",
+        "name": "Abu Hasan",
+        "phone": "01*******",
+        "email": "abuhasan@gmail.com",
+        "salary": "10000",
+        "image": "http://"
+    },
+    {
+        "id":"3",
+        "name": "aoyan saha",
+        "phone": "01*******",
+        "email": "aoyansaha@gmail.com",
+        "salary": "1500",
+        "image": "http://"
+     
+    },
+    {
+        "id":"4",
+        "name": "tusar",
+        "phone": "01*******",
+        "email": "tusar@gmail.com",
+        "salary": "10500",
+        "image": "http://"
+     
+    },
+    {
+        "id":"5",
+        "name": "Intiaz",
+        "phone": "017******",
+        "email": "intiaz@gmail.com",
+        "salary": "2600",
+        "image": "http://"
+     
+    },
+    {
+        "id": "6",
+        "name": "Khair Ahamed",
+        "phone": "01*******",
+        "email": "khair@gmail.com",
+        "salary": "5500",
+        "image": "http://"
+     
+    },
+    {
+        "id":"7",
+        "name": "mahi",
+        "phone": "01*******",
+        "email": "mahi@gmail.com",
+        "salary": "10800",
+        "image": "http://"
+     
+    },
+    {
+        "id":"8",
+        "name": "Shorif",
+        "phone": "01******",
+        "email": "shorif@gmail.com",
+        "salary": "18100",
+        "image": "http://"
+     
+    },
+    {
+        "id":"9",
+        "name": "Alomgir",
+        "phone": "01*******",
+        "email": "alomgir@gmail.com",
+        "salary": "13500",
+        "image": "http://"
+     
+    },
+    {
+        "id":"10",
+        "name": "al-amin",
+        "phone": "01******21",
+        "email": "alamin@gmail.com",
+        "salary": "15300",
+        "image": "http://"
+     
+    },
+    {
+        "id": "11",
+        "name": "Tanzim",
+        "phone": "01***44***",
+        "email": "tanzim@gmail.com",
+        "salary": "10300",
+        "image": "http://"
+     
+    }
+    {
+        "id":"12",
+        "name": "Opi",
+        "phone": "01****32*",
+        "email": "opi@gmail.com",
+        "salary": "1800",
+        "image": "http://"
+     
+    },
+    {
+        "id":"13",
+        "name": "Salmun",
+        "phone": "01**11***",
+        "email": "salmun@gmail.com",
+        "salary": "1300",
+        "image": "http://"
+     
+    },
+    {
+        "id":"14",
+        "name": "Kawsar",
+        "phone": "01**3***21",
+        "email": "kawsar@gmail.com",
+        "salary": "1500",
+        "image": "http://"
+     
+    },
+    {
+        "id": "15",
+        "name": "Rakan",
+        "phone": "01***44***",
+        "email": "rakan@gmail.com",
+        "salary": "1900",
+        "image": "http://"
+     
+    }
+]
